dce5b1c049c2a36cfe02e042a9e04c8d0d4ada44
Anthony Hadwan <anthony.hadwan@hec.edu>
2023-08-27 10:08:40 +0200

    added improved dates

--- app/news/components/NewsCard.js
+++ app/news/components/NewsCard.js
@@ -15,6 +15,24 @@ export default function NewsCard({
   contentSnippet,
   source,
 }) {
+  const currentTime = new Date().getTime();
+  const timeDifference = currentTime - pubDate;
+  const minsAgo = Math.floor(timeDifference / (1000 * 60));
+  const hoursAgo = Math.floor(minsAgo / 60);
+
+  let timeAgoText = '';
+  if (hoursAgo === 0) {
+    if (minsAgo <= 1) {
+      timeAgoText = 'Just now';
+    } else {
+      timeAgoText = `${minsAgo} minutes ago`;
+    }
+  } else if (hoursAgo === 1) {
+    timeAgoText = '1 hour ago';
+  } else {
+    timeAgoText = `${hoursAgo} hours ago`;
+  }
+
   return (
     <Link href={link} target="_blank">
       <Card className="bg-slate-700">
@@ -22,7 +40,7 @@ export default function NewsCard({
           <CardTitle>{title}</CardTitle>
           <CardDescription>{contentSnippet}</CardDescription>
           <CardFooter>
-            {source} | {new Date(pubDate).toLocaleTimeString()}
+            {source} | {timeAgoText}
           </CardFooter>
         </CardHeader>
       </Card>
